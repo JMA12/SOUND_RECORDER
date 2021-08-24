307f554f319073b20832b9ce3beb0e42882263a8
onemykiel <OneMykiel@gmail.com>
2021-08-24 23:16:20 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import styled from 'styled-components'
 import './App.css';
+
+const StyledAppContainer = styled.div`
+  padding: 90px 40px;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledAppContainer className="App">
+      <h1 className="title">
+        SOUND RECORDER
+      </h1>
+    </StyledAppContainer>
   );
 }
 
